910341730ccf410a6a23eb8025845048158d5e1f
Vicluar Daniel Merchan Briceno <vicluar@gmail.com>
2020-03-27 00:15:20 +0100

    Adding a unit tests project for the Web App.

---
+++ Retry.Circuit.Breaker.ASP.NET.Core/Resilient.WebApp.UnitTests/ServicesTests/WeatherForecastServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Resilient.WebApp.Services;
+
+namespace Resilient.WebApp.UnitTests.ServicesTests
+{
+    public class WeatherForecastServiceTests
+    {
+        private readonly IWheaterForecastService _sut;
+
+        public WeatherForecastServiceTests()
+        {
+            _sut = new WeatherForecastService();
+        }
+
+        [Test]
+        public void Do_Return_A_List_Of_Weather_Forecasts()
+        {
+            var forecasts = _sut.GetForecasts();
+
+            Assert.IsNotNull(forecasts);
+            Assert.IsTrue(forecasts.Count() > 0);
+        }
+    }
+}
--- Retry.Circuit.Breaker.ASP.NET.Core/Resilient.WebApp/Services/IWheaterForecastService.cs
+++ Retry.Circuit.Breaker.ASP.NET.Core/Resilient.WebApp/Services/IWheaterForecastService.cs
@@ -1,8 +1,5 @@
 ﻿using Resilient.WebApp.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Resilient.WebApp.Services
 {
